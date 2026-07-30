c3ae44b10eeb9a21fdf554dd3c59248a3723d4f9
BiaHoi-BaChien <sugi@clb-biahoi.net>
2026-07-30 15:22:59 +0700

    feat: add Lucky Wallpaper site icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="{{ asset('site-icon.png') }}" type="image/png" sizes="180x180">
+        <link rel="icon" href="{{ asset('site-icon.svg') }}" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="{{ asset('site-icon.png') }}">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
